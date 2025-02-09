4727ad70ed274a7eb3499f811443086297c5f923
Fries-Byte <69freshfrenchfries@gmail.com>
2025-02-09 21:51:29 +0100

    Update package.py

--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 [""" Package """]
 import urllib.request
-version = 9
+version = 10
 name = "versions"
 exec(urllib.request.urlopen("https://raw.githubusercontent.com/Fries-byte/pistud/refs/heads/main/packages.py").read().decode())
 
